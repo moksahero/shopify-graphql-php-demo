b65a1da143ae6ee0892364c91f715bef6dfaf724
Takeshi Amano <moksahero@gmail.com>
2018-10-15 15:20:48 +0900

    add graphql caller function

---
+++ graphql.php
@@ -0,0 +1,19 @@
+<?php
+$store_host = 'https://devmetokyo.myshopify.com';
+
+$query = '{ shop { name } }';
+
+$options = [
+  'http' => [
+    'method' => 'POST',
+    'header' => [
+      'Content-Type: application/graphql',
+      'X-Shopify-Access-Token: ' . '787034afe88f89aed6f6779020ee647e'
+    ],
+    'content' => $query
+  ]
+];
+
+$context = stream_context_create($options);
+$contents = file_get_contents($store_host . '/admin/api/graphql.json', false, $context);
+print_r(json_decode($contents));
